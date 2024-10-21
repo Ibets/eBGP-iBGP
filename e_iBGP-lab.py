800a4aea99ffcaf1913ffaaa58e13c5d7e3ca18f
Ibets <91409909+Ibets@users.noreply.github.com>
2024-10-21 11:23:39 +0700

    Update e_iBGP-lab.py

--- e_iBGP-lab.py
+++ e_iBGP-lab.py
@@ -116,34 +116,35 @@ class OSPFLab(Topo):
 						( '/var/run' ),
 						'/var/mn' ]
 		
-		#  R1 subnet
-		C1_1 = self.addHost('C1_1', ip="172.16.1.2/24", defaultRoute="via 172.16.1.1")
-		C1_2 = self.addHost('C1_2', ip="172.16.2.2/24", defaultRoute="via 172.16.2.1")
-		R1 = self.addNode("R1", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
-		S1 = self.addSwitch("S1", inNamespace=True)
-		R1_1 = self.addNode("R1_1", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
-		R1_2 = self.addNode("R1_2", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		#  AS1
+		C11 = self.addHost('C11', ip="172.16.1.2/24", defaultRoute="via 172.16.1.1")
+		
+		R11 = self.addNode("R11", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		R12 = self.addNode("R11", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		R13 = self.addNode("R13", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		R14 = self.addNode("R14", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		S11 = self.addSwitch("S11", inNamespace=True)
 
+		self.addLink(R11,R2, intfName1="eth1", intfName2="eth1")
+		self.addLink(R11,R3, intfName1="eth2", intfName2="eth1")
+		self.addLink(R2,R3, intfName1="eth2", intfName2="eth2")
 		# add links for subnet 1
-		self.addLink(S1,R1, intfName2="eth0") 
-		self.addLink(S1,R1_1, intfName2="eth0")
+		self.addLink(S11,R11, intfName2="eth0") 
+		self.addLink(S11,C11)
 		self.addLink(S1,R1_2,  intfName2="eth0")
-	#region
+	        #region
 		# Do not manually set the interface name of a switch's interface
 		# mininet will not be able to automatically add the interfaces to its bridge
 		#endregion
 
-		self.addLink(C1_1,R1_1, intfName2="eth1")
-		self.addLink(C1_2,R1_2, intfName2="eth1")
-
-		# R2 Subnet
+		# AS2 Subnet
 		C2_1 = self.addHost('C2_1', ip="172.17.1.2/24", defaultRoute="via 172.17.1.1")
 		C2_2 = self.addHost('C2_2', ip="172.17.2.2/24", defaultRoute="via 172.17.2.1")
-		R2 = self.addNode("R2", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		R21 = self.addNode("R2", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
 		S2 = self.addSwitch("S2", inNamespace=True)
-		
-		R2_1 = self.addNode("R2_1", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
-		R2_2 = self.addNode("R2_2", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		R22 = self.addNode("R2_1", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		R23 = self.addNode("R2_2", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
+		R24 = self.addNode("R2_2", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
 
 		# add links for subnet 2
 		self.addLink(S2,R2, intfName2="eth0")
@@ -152,7 +153,7 @@ class OSPFLab(Topo):
 		self.addLink(C2_1,R2_1, intfName2="eth1")
 		self.addLink(C2_2,R2_2, intfName2="eth1")
 
-# R3 Subnet
+		# AS3 Subnet
 		C3_1 = self.addHost('C3_1', ip="172.18.1.2/24", defaultRoute="via 172.18.1.1")
 		C3_2 = self.addHost('C3_2', ip="172.18.2.2/24", defaultRoute="via 172.18.2.1")
 		R3 = self.addNode("R3", cls=LinuxRouter, ip=None, privateDirs=privateDirs, inNamespace=True)
